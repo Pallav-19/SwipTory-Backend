410b27a8e1721f406407419f26a68b5d9dcdf6c5
Pallav <pallavinvizag@gmail.com>
2023-09-09 22:41:14 +0530

    add story bug

--- controllers/story/createStory.js
+++ controllers/story/createStory.js
@@ -1,16 +1,22 @@
-const Story = require("../../models/stories")
+const Story = require("../../models/stories");
 
 const createStory = async (req, res) => {
     console.log(req.body);
     try {
-        if (req.body.stories < 3) return res.status(204).json({ message: "Minimum 3 Stories are required!" })
-        stories = (req?.body?.stories?.map(s => ({ ...s, createdBy: req.user.userId })))
-        const newStory = await Story.insertMany(stories).populate([{ path: "createdBy", select: "username" }])
-        res.json({ message: "Story created successfully!", result: newStory })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Internal error occured" })
-    }
+        if (req.body.stories.length < 3) {
+            return res.status(400).json({ message: "Minimum 3 Stories are required!" });
+        }
+        const stories = req.body.stories.map((s) => ({ ...s, createdBy: req.user.userId }));
+        const newStories = await Story.insertMany(stories);
+        const populatedStories = await Story.find({ _id: { $in: newStories.map((story) => story._id) } })
+            .populate({ path: "createdBy", select: "username" })
+            .exec();
 
-}
-module.exports = createStory
+        res.json({ message: "Stories created successfully!", result: populatedStories });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal error occurred" });
+    }
+};
+
+module.exports = createStory;
