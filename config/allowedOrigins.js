1c550cae1a0f9005ffeddfc55096f7d6b51b1d61
Pallav Kumar Patra <73886524+Pallav-19@users.noreply.github.com>
2024-07-28 23:16:19 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,6 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     "https://swiptory-frontend-black.vercel.app",
-    "swiptory.pallav.site"
+    "https://swiptory.pallav.site"
 ]
 module.exports = allowedOrigins
