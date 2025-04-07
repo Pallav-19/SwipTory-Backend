a0194bf38da18a53b41148ca85a6d0fd2ee04263
pallav-kp19 <pallav.patra.1912@gmail.com>
2025-04-08 00:22:09 +0530

    MONGO_URI

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,9 @@
+require("dotenv").config()
+
 const mongoose = require('mongoose')
 const connectDB = async () => {
     return new Promise((resolve, reject) => {
-        mongoose.connect("mongodb+srv://pallav:njokWK5sqaMAyBKB@cluster0.fr9ili9.mongodb.net/swiptory?retryWrites=true&w=majority").then(x => resolve("Connected to atlas")).catch(err => console.log(err))
+        mongoose.connect(process.env.MONGO_URI).then(x => resolve("Connected to atlas")).catch(err => console.log(err))
 
     })
 }
